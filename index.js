e30f4276ebbafc239cf6708fdc445d6ca364987e
David <davidafarmer@gmail.com>
2023-04-23 14:52:30 -0400

    Array filter Method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+
+
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (drivers, input) => {
+  return drivers.filter(driver => driver.toLowerCase() === input.toLowerCase());
+}
+
+const fuzzyMatch = (drivers, input) => {
+    return drivers.filter(driver => driver.startsWith(input));
+  }
+
+  const matchName = (drivers, input) => {
+    return drivers.filter(driver => driver.name === input);
+  }
